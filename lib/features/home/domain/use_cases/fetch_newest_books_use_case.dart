34983a96940cb37899645c2dfc9da3a8a42b741e
kerolosragaie <kerolosragaie@gmail.com>
2023-05-02 14:16:12 +0200

    FetchNewestBooks Usecase

---
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookilo_clean_arch/core/errors/failures.dart';
+import 'package:bookilo_clean_arch/core/use_cases/use_case.dart';
+import 'package:bookilo_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:bookilo_clean_arch/features/home/domain/repositories/home_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUsecase extends Usecase<List<BookEntity>, NoParam> {
+  final HomeRepository homeRepository;
+
+  FetchNewestBooksUsecase({required this.homeRepository});
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepository.fetchNewestBooks();
+  }
+}
