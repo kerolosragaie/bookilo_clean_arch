1cbf1400c882d1d061170a4d5b521970ac6d9da9
kerolosragaie <kerolosragaie@gmail.com>
2023-05-02 13:39:13 +0200

    adding either

--- lib/features/home/domain/repositories/home_repository.dart
+++ lib/features/home/domain/repositories/home_repository.dart
@@ -1,6 +1,8 @@
+import 'package:bookilo_clean_arch/core/errors/failures.dart';
 import 'package:bookilo_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepository {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
