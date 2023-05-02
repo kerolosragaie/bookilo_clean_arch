95682bd5bac7b42e72bb0919ac07ae9c7683e560
kerolosragaie <kerolosragaie@gmail.com>
2023-05-02 13:55:53 +0200

    create fetch featured books usecase

---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:bookilo_clean_arch/features/home/domain/repositories/home_repository.dart';
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+import '../entities/book_entity.dart';
+
+class FetchFeaturedBooksUsecase {
+  final HomeRepository homeRepository;
+
+  FetchFeaturedBooksUsecase({required this.homeRepository});
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    //ex: check internet permission
+    return homeRepository.fetchFeaturedBooks();
+  }
+}
