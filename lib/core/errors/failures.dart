392e4d63f7ce6c32687b830bee242543d2d26a8d
kerolosragaie <kerolosragaie@gmail.com>
2023-05-03 18:11:47 +0200

    done home repo impl and failure handling

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,58 +1,76 @@
 import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 abstract class Failure {
-  final String errorMessage;
-  const Failure({required this.errorMessage});
+  final String? message;
+  const Failure({this.message});
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure({required super.errorMessage});
+  const ServerFailure({required super.message});
 
   factory ServerFailure.fromDioError(DioError dioError) {
     switch (dioError.type) {
       case DioErrorType.connectionTimeout:
-        return const ServerFailure(
-            errorMessage: "Connection timeout with server!");
+        return const ServerFailure(message: "Connection timeout with server!");
       case DioErrorType.sendTimeout:
-        return const ServerFailure(
-            errorMessage: "Send timeout, please try again later!");
+        return const ServerFailure(message: "Send timeout with API server!");
       case DioErrorType.receiveTimeout:
-        return const ServerFailure(
-            errorMessage: "Receive timeout, please try again later!");
+        return const ServerFailure(message: "Receive timeout with API server!");
       case DioErrorType.badCertificate:
-        return const ServerFailure(errorMessage: "Incorrect certificate.");
+        return const ServerFailure(message: "Incorrect certificate.");
       case DioErrorType.badResponse:
         return ServerFailure.fromResponse(
             dioError.response!.statusCode!, dioError.response!.data);
       case DioErrorType.cancel:
-        return const ServerFailure(errorMessage: "Cancelled connection");
-      case DioErrorType.connectionError:
-        return const ServerFailure(errorMessage: "Connection error");
-      case DioErrorType.unknown:
         return const ServerFailure(
-            errorMessage: "Unkown error, please try again!");
+            message: "Request to API server was cancelled.");
+      case DioErrorType.connectionError:
+        return const ServerFailure(message: "No internet connection.");
+      case DioErrorType.unknown:
+        return const ServerFailure(message: "Unkown error, please try again!");
       default:
         return const ServerFailure(
-            errorMessage: "Opps, unkown error, please try again later!");
+            message: "Opps, unkown error, please try again later!");
     }
   }
 
   factory ServerFailure.fromResponse(int statusCode, dynamic reponse) {
     if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
-      return ServerFailure(errorMessage: reponse["error"]["message"]);
+      return ServerFailure(message: reponse["error"]["message"]);
     } else if (statusCode == 404) {
       return const ServerFailure(
-          errorMessage: "Your request not found, please try again later!");
+          message: "Your request not found, please try again later!");
     } else if (statusCode == 500) {
       return const ServerFailure(
-          errorMessage: "Internal server error, please try again later!");
+          message: "Internal server error, please try again later!");
     } else {
       return const ServerFailure(
-          errorMessage: "Opps, unkown error, please try again later!");
+          message: "Opps, unkown error, please try again later!");
     }
   }
 }
 
-class OfflineFailure extends Failure {
-  const OfflineFailure({required super.errorMessage});
+class NetworkFailure extends Failure {
+  NetworkFailure({
+    super.message,
+  });
+  factory NetworkFailure.fromInternetConnection(
+      InternetConnectionStatus internetConnectionStatus) {
+    return NetworkFailure(
+        message:
+            "Cannot connect to internet, please check your connection and try again.");
+  }
+}
+
+class CacheFailure extends Failure {
+  CacheFailure({required super.message});
+  factory CacheFailure.fromHiveError(HiveError hiveError) {
+    return CacheFailure(message: hiveError.message);
+  }
+}
+
+class UnkownFailure extends Failure {
+  UnkownFailure({required super.message});
 }
--- lib/features/home/data/repositories/home_repository_impl.dart
+++ lib/features/home/data/repositories/home_repository_impl.dart
@@ -4,6 +4,9 @@ import 'package:bookilo_clean_arch/features/home/domain/entities/book_entity.dar
 import 'package:bookilo_clean_arch/core/errors/failures.dart';
 import 'package:bookilo_clean_arch/features/home/domain/repositories/home_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class HomeRepositoryImpl extends HomeRepository {
   final HomeRemoteDatasource homeRemoteDatasource;
@@ -24,7 +27,7 @@ class HomeRepositoryImpl extends HomeRepository {
       booksList = await homeRemoteDatasource.fetchFeaturedBooks();
       return Right(booksList);
     } catch (e) {
-      return Left(ServerFailure(errorMessage: e.runtimeType.toString()));
+      return _catchingError(e);
     }
   }
 
@@ -39,7 +42,18 @@ class HomeRepositoryImpl extends HomeRepository {
       booksList = await homeRemoteDatasource.fetchNewestBooks();
       return Right(booksList);
     } catch (e) {
-      return Left(ServerFailure(errorMessage: e.runtimeType.toString()));
+      return _catchingError(e);
     }
+  }
+
+  Left<Failure, List<BookEntity>> _catchingError(Object e) {
+    if (e is HiveError) {
+      return Left(CacheFailure.fromHiveError(e));
+    } else if (e is DioError) {
+      return Left(ServerFailure.fromDioError(e));
+    } else if (e is InternetConnectionStatus) {
+      return Left(NetworkFailure.fromInternetConnection(e));
+    }
+    return Left(UnkownFailure(message: e.toString()));
   }
 }
