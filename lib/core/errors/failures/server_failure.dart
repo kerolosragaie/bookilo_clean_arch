7ec576a3dd37b4498ae48307e0b762f5568eaa64
kerolosragaie <kerolosragaie@gmail.com>
2023-06-17 14:34:00 +0200

    updating dio error types

--- lib/core/errors/failures/server_failure.dart
+++ lib/core/errors/failures/server_failure.dart
@@ -3,27 +3,27 @@ part of 'failures.dart';
 class ServerFailure extends Failure {
   const ServerFailure({required super.message});
 
-  factory ServerFailure.fromDioError(DioError dioError) {
+  factory ServerFailure.fromDioError(DioException dioError) {
     switch (dioError.type) {
-      case DioErrorType.connectionTimeout:
+      case DioExceptionType.connectionTimeout:
         return const ServerFailure(message: "Connection timeout with server!");
-      case DioErrorType.sendTimeout:
+      case DioExceptionType.sendTimeout:
         return const ServerFailure(message: "Send timeout with API server!");
-      case DioErrorType.receiveTimeout:
+      case DioExceptionType.receiveTimeout:
         return const ServerFailure(message: "Receive timeout with API server!");
-      case DioErrorType.badCertificate:
+      case DioExceptionType.badCertificate:
         return const ServerFailure(message: "Incorrect certificate.");
-      case DioErrorType.badResponse:
+      case DioExceptionType.badResponse:
         return ServerFailure.fromResponse(
           dioError.response!.statusCode!,
           dioError.response!.data,
         );
-      case DioErrorType.cancel:
+      case DioExceptionType.cancel:
         return const ServerFailure(
             message: "Request to API server was cancelled.");
-      case DioErrorType.connectionError:
+      case DioExceptionType.connectionError:
         return const ServerFailure(message: "No internet connection.");
-      case DioErrorType.unknown:
+      case DioExceptionType.unknown:
         return const ServerFailure(message: "Unkown error, please try again!");
       default:
         return const ServerFailure(
@@ -31,10 +31,10 @@ class ServerFailure extends Failure {
     }
   }
 
-  factory ServerFailure.fromResponse(int statusCode, dynamic reponse) {
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
     if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
       //?The message Depend on API
-      return ServerFailure(message: reponse["error"]["message"]);
+      return ServerFailure(message: response["error"]["message"]);
     } else if (statusCode == 404) {
       return const ServerFailure(
           message: "Your request not found, please try again later!");
